97a8cbf6aec01f5bf4fa0a403e602024f8b5d2d5
brodyzt <brodyzt@gmail.com>
2017-11-14 13:56:20 -0500

    Added comments for linear algebra mli

--- linear_alg.mli
+++ linear_alg.mli
@@ -2,16 +2,23 @@
 
 type matrix
 
+(* [dot_product a b] is a•b where a and b are both matrices *)
 val dot_product : matrix -> matrix -> matrix
 
+(* [cross_product a b] is axb where a and b are both matrices *)
 val cross_product : matrix -> matrix -> matrix
 
+(* [scale a b] is the matrix a scaled by factor b *)
 val scale: matrix -> number -> matrix
 
+(* [inverse a] is the inverse matrix of matrix a *)
 val inverse: matrix -> matrix
 
+(* [transpose a] is matrix a transposed *)
 val transpose: matrix -> matrix
 
+(* [add a b] is the sum of matrices a and b *)
 val add: matrix -> matrix -> matrix
 
+(* [add a b] is the difference of matrices a and b *)
 val subtract: matrix -> matrix -> matrix

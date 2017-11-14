1413461a0ce427e4c21b93d31085d9cdab9ac8ab
martineizayaga <martineizayaga@gmail.com>
2017-11-13 22:23:08 -0500

    Created combinatorics.mli
    added three methods

---
+++ combinatorics.mli
@@ -0,0 +1,8 @@
+(* [factorial i] is the i! *)
+val factorial : integer -> integer
+
+(* [permutation n r] is nPr *) 
+val permutation : integer -> integer -> integer
+
+(* [combination n r] is nCr *)
+val combination : integer -> integer -> integer

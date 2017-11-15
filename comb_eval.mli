aec6e755f56f7a6a5b0b84866ef0857abb5d5e8c
Eric Landgrebe <elandgre@gmail.com>
2017-11-15 16:45:40 -0500

    changes to combinatorics mli

--- comb_eval.mli
+++ comb_eval.mli
@@ -1,10 +1,11 @@
-
-(*[factorial n] computes the matmatical factorial of [n], if [n] is negative
+include Eval
+(*[factorial n] computes the mathematical factorial of [n], if [n] is negative
  *then the result of the factorial will be an exn*)
 val factorial: integer -> value
 
-(*[combination a b] commputes the matmatical formula for [a] choose [b] where
- * if [a] and [b] are negative then the result of the factorial will be an exn*)
+(*[combination a b] computes the mathematical formula for [a] choose [b] where
+ * if either of the [a] and [b] are negative then the result
+  of the factorial will be an exn*)
 val combination: integer -> integer -> value
 
 (*[partition_identical a b] counts the number of ways to partition [a]
@@ -12,9 +13,8 @@ val combination: integer -> integer -> value
  * result of the factorial will be an exn*)
 val partition_identical : integer -> integer -> value
 
-(*[permutation a b] commputes the matmatical formula for [a] ! ( [a] − [b] ) !
+(*[permutation a b] commputes the mathematical formula for [a]!/([a] − [b])!
  *where if [a] and [b] are negative then the absolute values are used and
  *computed if [a] and [b] are negative then the result of the factorial will
  * be an exn*)
 val permutation: integer -> integer -> value
-

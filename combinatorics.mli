c22a9d835b69ba2d7f62638583130c6d13bcf9cb
Eric Landgrebe <elandgre@gmail.com>
2017-11-15 15:39:16 -0500

    removed redundant combinatorics module

--- combinatorics.mli
+++
@@ -1,8 +0,0 @@
-(* [factorial i] is the i! *)
-val factorial : integer -> integer
-
-(* [permutation n r] is nPr *) 
-val permutation : integer -> integer -> integer
-
-(* [combination n r] is nCr *)
-val combination : integer -> integer -> integer

0cbda3bf78a943f02cb6d22a4fd1e107f28609b4
Katie Mancini <klm299@cornell.edu>
2017-11-15 15:34:31 -0500

    adding exn

--- comb_eval.mli
+++ comb_eval.mli
@@ -1,13 +1,20 @@
 
-(*[factorial n] computes the matmatical factorial of [n], if n is negative then
- * the factorial will be the negated factorial of the absolute value of [n]*)
-val factorial: integer -> integer
+(*[factorial n] computes the matmatical factorial of [n], if [n] is negative
+ *then the result of the factorial will be an exn*)
+val factorial: integer -> value
 
 (*[combination a b] commputes the matmatical formula for [a] choose [b] where
- * if [a] and [b] are negative then the absolute values are used and computed*)
-val combination: integer -> integer -> integer
+ * if [a] and [b] are negative then the result of the factorial will be an exn*)
+val combination: integer -> integer -> value
+
+(*[partition_identical a b] counts the number of ways to partition [a]
+ * identical elements into [b] catagories if [a] and [b] are negative then the
+ * result of the factorial will be an exn*)
+val partition_identical : integer -> integer -> value
 
 (*[permutation a b] commputes the matmatical formula for [a] ! ( [a] − [b] ) !
  *where if [a] and [b] are negative then the absolute values are used and
- *computed*)
-val permutation: integer -> integer -> integer
+ *computed if [a] and [b] are negative then the result of the factorial will
+ * be an exn*)
+val permutation: integer -> integer -> value
+
--- eval.mli
+++ eval.mli
@@ -7,11 +7,18 @@ type number = I of integer | F of float
 (*this is the type of a function in the languge*)
 type function
 
+(*this type will represent an exception in the language*)
+type exn
+
 (*the values of the language will be either strings, numbers or matricies*)
-type value = S of string | N of number | M of Linear_alg.matrix;
+type value = S of string | N of number | M of Linear_alg.matrix | E of exn;
 
 (*this is the enviment with all of the function bindings*)
 type env
+
+(*this will be the type for the stack that we will use to store the values
+ *on*)
+type stack
 
 (*[evaluate_line s] evaluates the string [s] to a value and then gives back a
  * string form of the value*)

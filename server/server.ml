0ea67962e0d8111f2a53fe959114cc3b68a2bff3
Katie Mancini <klm299@cornell.edu>
2017-11-25 17:24:58 -0500

    server is working

--- server/server.ml
+++ server/server.ml
@@ -11,7 +11,7 @@ let test req =
   {headers; status; res_body}
 
 let webhook req =
-  let headers = Header.init_with "Content-Type" "text/plain" in
+  let headers = Header.init_with "Content-Type" "application/json" in
   let j = Yojson.Basic.from_string req.req_body in
     if (j |> member "object" |> to_string) = "page" then
       let status = `OK in
@@ -22,9 +22,35 @@ let webhook req =
       let res_body = "sad times" in
         {headers; status; res_body}
 
+let webhook_verif req =
+  let headers = Header.init_with "Content-Type" "text/plain" in
+  let verify_token = "erybcnasdgkadsda" in
+    if List.mem_assoc "hub.mode" req.params
+    && List.mem_assoc "hub.verify_token" req.params
+    && List.mem_assoc "hub.challenge" req.params
+    then
+      let mode = List.assoc "hub.mode" req.params in
+      let token = List.assoc "hub.verify_token" req.params in
+      let challenge = List.assoc "hub.challenge" req.params in
+
+      if mode = "subscribe" && token = verify_token then
+        (print_string "WEBHOOK VERIFIED!!!!!!";
+        let status = `OK in
+        let res_body = challenge in
+          {headers; status; res_body})
+      else
+        let status = `Unauthorized in
+        let res_body = "sad times" in
+          {headers; status; res_body}
+    else
+      let status = `Unauthorized in
+      let res_body = "sad times" in
+        {headers; status; res_body}
+
 
 let _ =
   HttpServer.add_route (`POST, "/test") test;
   HttpServer.add_route (`GET, "/test/get") test;
   HttpServer.add_route (`POST, "/webhook") webhook;
+  HttpServer.add_route (`GET, "/webhook") webhook_verif;
   HttpServer.run ~port:8000 ()

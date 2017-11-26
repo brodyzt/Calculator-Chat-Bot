41b4092670b1dd4c555399703b1762f259668e2d
martineizayaga <martineizayaga@gmail.com>
2017-11-26 00:15:12 -0500

    simpl_arith and comb_eval implemented

--- comb_eval.mli
+++ comb_eval.mli
@@ -4,17 +4,17 @@
 val factorial: Types.integer -> Types.value
 
 (*[combination a b] computes the mathematical formula for [a] choose [b] where
- * if either of the [a] and [b] are negative then the result
+ * if either of the [a] or [b] are negative then the result
   of the factorial will be an exn*)
 val combination: Types.integer -> Types.integer -> Types.value
 
 (*[partition_identical a b] counts the number of ways to partition [a]
- * identical elements into [b] catagories if [a] and [b] are negative then the
+ * identical elements into [b] catagories if [a] or [b] are negative then the
  * result of the factorial will be an exn*)
 val partition_identical : Types.integer -> Types.integer -> Types.value
 
 (*[permutation a b] commputes the mathematical formula for [a]!/([a] − [b])!
  *where if [a] and [b] are negative then the absolute values are used and
- *computed if [a] and [b] are negative then the result of the factorial will
+ *computed if [a] or [b] are negative then the result of the factorial will
  * be an exn*)
 val permutation: Types.integer -> Types.integer -> Types.value

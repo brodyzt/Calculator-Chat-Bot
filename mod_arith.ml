3fb4ff9fbfc0505bd280502d75a8c78ca99e305b
Eric Landgrebe <elandgre@gmail.com>
2017-11-26 02:19:37 -0600

    added really bad factoring

--- mod_arith.ml
+++ mod_arith.ml
@@ -46,7 +46,24 @@ let lcm a b =
 
 let gen_prime l = N(I l)
 
-let factor n = N(I n)
+(*TODO: Make tail recursive*)
+let rec add_factor lst fact =
+  match lst with
+  | [] -> [(fact,big_int_of_int 1)]
+  | (value,freq)::t ->
+    if ((compare_big_int value fact) = 0)
+    then (value,(add_big_int freq (big_int_of_int 1)))::t
+    else (value,freq)::(add_factor t fact)
+
+(*NOTE: Time complexity has to be improved*)
+let rec factor_helper n d accum =
+  if ((compare_big_int d n) > 0) then accum
+  else let r = mod_big_int n d in
+    if ((compare_big_int r zero_big_int)=0)
+    then factor_helper (div_big_int n d) d (add_factor accum d)
+    else factor_helper n (add_big_int d (big_int_of_int 1)) accum
+
+let factor n = Fact(factor_helper n (big_int_of_int 2) [])
 
 let is_prime n = N(I n)
 

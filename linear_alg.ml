f8986d28f7fb7f7dfc544542098d4374f32d3517
Katie Mancini <klm299@cornell.edu>
2017-11-30 11:50:03 -0500

    det and eq

--- linear_alg.ml
+++ linear_alg.ml
@@ -110,23 +110,25 @@ let string_of_matrix m =
     "]"
      )
 
-let rec red_row_down m i j=
+let rec red_row_down m i j =
   let rows = Array.length m in
   let cols = Array.length (m.(0)) in
     if i < rows && j < cols then
       if m.(i).(j) = F( 0.) then
         let non_zero = find_non_zero m (i+1) j in
           if non_zero = -1 then red_row_down m i (j+1)
-          else (swap m i non_zero; red_row_down m i j)
+          else
+            let (rr,s) = (swap m i non_zero; red_row_down m i j) in
+              (rr,s+1)
       else
         (Array.iteri (fun ind row -> if ind > i then m.(ind) <- (clear_col j ( m.(i) ) row ) else () ) m;
         print_string (string_of_matrix m);
         red_row_down m (i+1) (j+1))
     else
-     M(m)
+     (M(m),0)
 
 let row_echelon m =
-  red_row_down (Array.map (fun row -> Array.copy row) m) 0 0
+  fst (red_row_down (Array.map (fun row -> Array.copy row) m) 0 0)
 
 let find_pivot m i =
   let rec piv j =
@@ -154,27 +156,51 @@ let read_off_sol a m =
   let len = Array.length a.(0) in
     Array.iteri (fun i row -> m.(i).(0) <- row.(len-1)) a; m
 
-
 let solve m1 m2 =
   let rows1 = Array.length m1 in
   let rows2 = Array.length m2 in
     if rows1 = 0 || Array.length m1.(0) = 0 || rows1 = rows2 then
-      let aux = Array.make_matrix rows1 ((Array.length m1.(0)) +1) (F 0.) in
-        Array.iteri (fun i row -> Array.iteri (fun j (F v) -> aux.(i).(j) <- m1.(i).(j)) row) m1;
-        Array.iteri (fun i row -> aux.(i).(Array.length m1.(0)) <- row.(0)) m2;
-          let M(sol) = red_row_echelon aux in
+      let aug = Array.make_matrix rows1 ((Array.length m1.(0)) +1) (F 0.) in
+        Array.iteri (fun i row -> Array.iteri (fun j (F v) -> aug.(i).(j) <- m1.(i).(j)) row) m1;
+        Array.iteri (fun i row -> aug.(i).(Array.length m1.(0)) <- row.(0)) m2;
+          let M(sol) = red_row_echelon aug in
             M(read_off_sol (sol) m2)
     else
      E "matrix size issue"
 
-let determinant m = N (I (Big_int.big_int_of_int 0))
+let rec prod_diag m i j acc =
+  let rows = Array.length m in
+  let cols = Array.length (m.(0)) in
+    if i < rows && j < cols then
+      match m.(i).(j) with
+      | F(f) -> prod_diag m (i+1) (j+1) (acc *. f)
+      | _ -> failwith "unimplemented"
+    else acc
 
-let lin_ind m = N (I (Big_int.big_int_of_int 0))
 
-let lin_dep m = N (I (Big_int.big_int_of_int 0))
+let determinant m =
+  let M(rr),swap_count = red_row_down m 0 0 in
+    N(F(prod_diag m 0 0 (if swap_count mod 2 = 1 then -1. else 1.)))
+
+
+let lin_ind m =
+  if determinant m = N(F(0.)) then N (I (Big_int.big_int_of_int 0)) else N (I (Big_int.big_int_of_int 1))
+
+let lin_dep m =
+  if determinant m = N(F(0.)) then N (I (Big_int.big_int_of_int 1)) else N (I (Big_int.big_int_of_int 0))
 
 let null_space m = M(m)
 
 let col_space m = M(m)
 
-let eq m1 m2 = N (I (Big_int.big_int_of_int 0))
+let eq m1 m2 =
+  let r1 = Array.length m1 in
+  let c1 = Array.length (m1.(0)) in
+  let r2 = Array.length m2 in
+  let c2 = Array.length (m2.(0)) in
+    if r1 = r2 && c1 = c2 then
+      let e = ref 1 in
+        Array.iter2 (fun r1 r2 -> Array.iter2 (fun v1 v2 -> if v1 = v2 then () else e := !e * 0) r1 r2) m1 m2;
+        N(I(big_int_of_int (!e)))
+    else
+      N(I(big_int_of_int (0)))

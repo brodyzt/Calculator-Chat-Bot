78559a34db857f9773b499532d6e5214dea64974
brodyzt <brodyzt@gmail.com>
2017-12-04 19:49:35 -0500

    updates

--- server.ml
+++ server.ml
@@ -114,6 +114,7 @@ let webhook req =
     let handle_entry entry = (
       let webhook_event = List.nth (entry |> member "messaging" |> to_list) 0 in
       let sender_psid = webhook_event |> member "sender" |> member "id" |> to_string in
+      print_endline "Message: " ^ (webhook_event |> member "message" |> Yojson.Basic.pretty_to_string);
       let command = webhook_event |> member "message" |> member "text" |> to_string in
       let (result, env') = (Eval.evaluate_line !env command) in
       (env := env';
